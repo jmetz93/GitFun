0cb44b58fe3ca06837c14018bafd07adc4eb732f
Jacob Metzinger <jacob.metzinger@yahoo.com>
2018-03-13 11:39:26 -0700

    Little Change

--- longestCommonPrefix.js
+++ longestCommonPrefix.js
@@ -44,11 +44,10 @@ var longestCommonPrefix = function(strs) {
     var longestPrefix = '';
     if (strs.length > 0) {
       longestPrefix = strs[0];
-      for (let i = 1; i < strs.length; i++) {
-        for (let j = 0; j < longestPrefix.length; j++) {
+      for (var i = 1; i < strs.length; i++) {
+        for (var j = 0; j < longestPrefix.length; j++) {
           if (strs[i][j] != longestPrefix[j]) {
             longestPrefix = longestPrefix.slice(0, j);
-            break;
           }
         }
       }
